d2a29ad715d552c9bc16f78711fe17514fb8c171
darioguardia <rimuu48@gmail.com>
2026-06-04 10:05:46 -0300

    first video done

---
+++ export-import-practice.js
@@ -0,0 +1,5 @@
+import { PracticeImportExport } from "./javascript-practice.js"
+//import PracticeImportExport from "./javascript-practice.js"
+
+const obj1 = new PracticeImportExport()
+obj1.learn()
--- javascript-practice.js
+++ javascript-practice.js
@@ -62,4 +62,112 @@ const getAge = () =>{
 }
 getAge() //doesnt show anything because it needs to be asigned
 const ageVal = getAge()
-console.log(ageVal)
+console.log(ageVal) 
+
+//ARITHMETICS OPERATORS
+
+console.log(2+5)
+console.log(3-2)
+console.log(10/3)
+console.log(5*7)
+console.log(10%2)
+
+
+//COMPARISSON OPERATORS
+console.log(3===3) //compares 2 numbers and if they are equals drops true
+console.log(3!==5) // same but if they are not equals
+console.log(86>72) //true
+console.log(86<72) //false
+console.log(86<=72) //false
+console.log(42>=8) //true
+console.log("test"==="test") 
+
+//logical operators
+console.log(3===3 && 4===4)
+console.log(3===5 || 86>5)
+console.log(!false)
+
+//conditioners
+
+if(3===4){
+    console.log("Numbers are equal")
+}else{
+    console.log("Numbers are not equal")
+}
+
+const a = 3
+const b = 4
+
+if(a==b){
+     console.log("Numbers are equal")
+}else if(a>b){
+     console.log("Number "+a+" is greater than "+b)
+}else{
+    console.log("Number "+b+" is greater than "+a)
+}
+
+//LOOPS
+for(let i=0;i<=10;i++){
+    console.log(i)
+}
+
+let usersData = ["Fata","Dario","Picu","Leito",20,true]
+
+for (const user of usersData) {
+   console.log(user) 
+}
+
+//ASYNC AWAIT 
+
+async function loadDataHotel () {
+    await fetch("www.googlel.com")
+}
+
+//CLASSES
+
+class LoginPage{
+
+    // constructor(){
+    //     this.nameValue = "Dario"
+    //     this.emailValue = "rimuu48@gmail.com"
+    // }
+
+     constructor(company){
+        this.nameValue = "Dario"
+        this.emailValue = "rimuu48@gmail.com"
+        this.company = company
+    }
+
+    //METHODES
+    login(){
+        console.log("Login Called")
+        console.log("Company name: "+this.company)
+
+    }
+
+    sayHello(){
+        console.log("HELLO")
+    }
+}
+
+//OBJECT
+
+const loginPageObject = new LoginPage()
+
+loginPageObject.sayHello()
+console.log(loginPageObject.nameValue)
+console.log(loginPageObject.emailValue)
+const loginPageObject2 = new LoginPage("motivus")
+console.log(loginPageObject2.company)
+loginPageObject2.login()
+
+
+//IMPORT EXPORT
+//export default class PracticeImportExport { //call it without {}
+export class PracticeImportExport {
+
+    learn(){
+        console.log("Javascript Practice")
+    }
+}
+
