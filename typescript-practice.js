986ab41619b808aa0489e7b392d4c7f6a6a7dbc7
darioguardia <rimuu48@gmail.com>
2026-06-05 09:40:56 -0300

    typescript test code

---
+++ typescript-practice.js
@@ -0,0 +1,7 @@
+"use strict";
+//TO RUN WE NEED TO TYPE IN TERMINAL tsc filename.ts, it will generate a js file that we can run. We have to do this everytime we make a change on this file
+//WE CAN ALSO RUN tsc filename.ts && node filename.js, it will run both comands
+const nameValue = "Motivus";
+console.log(nameValue);
+let url = "www.google.com";
+console.log(url);
---
+++ typescript-practice.ts
@@ -0,0 +1,7 @@
+//TO RUN WE NEED TO TYPE IN TERMINAL tsc filename.ts, it will generate a js file that we can run. We have to do this everytime we make a change on this file
+//WE CAN ALSO RUN tsc filename.ts && node filename.js, it will run both comands
+const nameValue:string = "Motivus"
+console.log(nameValue)
+
+let url:string = "www.google.com"
+console.log(url)
