c822728e7ad17a2645b1262ef2c31c37ad392b47
darioguardia <rimuu48@gmail.com>
2026-06-07 17:11:16 -0300

    locators practice done

--- tests/kickStart.spec.ts
+++ tests/kickStart.spec.ts
@@ -1,4 +1,4 @@
-import {test} from "@playwright/test"
+import {test,expect} from "@playwright/test"
 
 
 //test("Kick Start with Playwright",()=>{  //arrow function means anonymous function
@@ -11,11 +11,57 @@ test("Kick Start with Playwright",async({page})=>{
 }) 
 
 
-test("Kick Start with Playwright2",()=>{ 
+test("Locator Practice",async({page})=>{ // https://playwright.dev/docs/locators 
+
+    await page.goto("https://www.saucedemo.com")
+    await expect(page).toHaveTitle(/Swag Labs/);
+
+    //XPATH
+    await page.locator("//input[@name='user-name']").fill("standard_user")
+
+    //CSS SELECTOR => htmltag.classvalue / .classvalue || htmltag#idValue / #idValue || htmltag[attributeName=attributeValue] / [attributeName=attributeValue]
+    await page.locator("input#password").fill("secret_sauce") //when is an id we use #
+    //await page.locator("input.submit-button").click() //when is a class we use the dot
+    await page.locator("input[value='Login']").click()
+
+    //TEXT LOCATOR -> text='textValue' or text=textValue, first one is case sensitive, 2nd one not
+    await page.locator("text='Sauce Labs Backpack'").click()
+    //await page.locator("text=Sauce LABS Backpack").click()
+
+    //CUSTOM PLAYWRIGHT=id|data-test|data-test-id|data-testid
+
+    // await page.locator("id=add-to-cart").click()
+   await page.locator("data-test=add-to-cart").click()
 
     console.log("Second Test")
 
 }) 
+
+test("Locator with Options", async({page})=>{
+
+    await page.goto("https://www.saucedemo.com")
+
+    //has locator
+    await page.locator("//div[@class='form_group']",{has:page.locator("//input[@name='user-name']")}).click()
+    await page.locator("//div[@class='form_group']",{has:page.locator("//input[@name='user-name']")}).pressSequentially("standard_user")
+    //await page.locator(".form_group",{has:page.locator("input#username")})
+
+    //hasNot locator
+    await page.locator("//div[@class='form_group']",{hasNot:page.locator("//input[@name='user-name']")}).click()
+    await page.locator("//div[@class='form_group']",{hasNot:page.locator("//input[@name='user-name']")}).pressSequentially("secret_sauce")
+   
+    //hasText Locator 
+    await page.locator("input[value='Login']").click()
+    await page.locator("//a",{hasText:"Sauce Labs Backpack"}).click()
+    await page.goBack()
+
+    //hasNotText Locator https://www.geeksforgeeks.org/javascript/javascript-regexpregular-expression/ 
+    await page.locator(".inventory_item_name",{hasNotText:/Sauce.*/}).click()
+
+    console.log("Third Test")
+
+
+})
 
 
 
